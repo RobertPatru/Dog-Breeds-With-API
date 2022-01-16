b9c7b0dbaf50b10f1b6a53dea6651f48f8a4b252
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-01-16 21:20:25 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ let imgID;
 selector.addEventListener('change', changeInfo);
 
 function changeInfo() {
+    selectDogId();
+
     // GET DOG'S INFORMATION
     fetch('https://api.thedogapi.com/v1/breeds?limit=10&page=0')
         .then(response => response.json())
@@ -26,27 +28,28 @@ function changeInfo() {
                     breedFor.textContent = 'Bred For: ' + data[i].bred_for;
                 }
             }
-        });
+        })
+        .catch( () => {
+            dogBreed.textContent = 'Name: ' + 'Nothing Data Found';
+            weight.textContent = 'Weight: ' + 'Nothing Data Found';
+            height.textContent = 'Height: ' + 'Nothing Data Found';
+            lifeSpan.textContent = 'Height: ' + 'Nothing Data Found';
+            breedFor.textContent = 'Bred For: ' + 'Nothing Data Found';
+    } );
 
-    selectDogId();
-
+    
     // GET DOG'S PICTURE
     fetch(`https://dog.ceo/api/breed/${imgID}/images/random`)
         .then(response => response.json())
-        .then(data => {
-            // daca nu gaseste imagine se strica toat functia, se opreste inainte sa intre in if-uri
-
-            if (data.staus == 'error') {
-                imageContainer.innerHTML = `<img src="/img/error.png">`;
-            } 
-            else {
-                imageContainer.innerHTML = `<img src=${data.message}>`
-            }           
-        });
-}
+        .then(data => imageContainer.innerHTML = `<img src=${data.message}>`)
+        .catch( (error) =>  {
+            console.log(error.statusCode);
+            console.log('Intra in catch!');
+    });
+}   // End f Chang Info Function
 
 
-    // GET DOG'S LIST and insert in select
+// GET DOG'S LIST and insert in select
 fetch('https://api.thedogapi.com/v1/breeds?limit=10&page=0')
     .then(response => response.json())
     .then(data => {
@@ -55,30 +58,30 @@ fetch('https://api.thedogapi.com/v1/breeds?limit=10&page=0')
             <option value="${data[i].name}">${data[i].name}</option>
         `;
         }
-      
-});
+    })
+    .catch( () => {
+        imageContainer.innerHTML = `<h1 class='error'>Dog list could not be loaded.</h1>`;
+        console.log('intra in catch');
+    });
 
 
 function selectDogId() {
-    console.log(selector.value);
     if(selector.value == 'Affenpinscher') {
-        console.log('merge');
         imgID = 'affenpinscher';
     }
     else if(selector.value == 'Afghan Hound') {
-        console.log('merge');
         imgID = 'hound';
     }
     else if(selector.value == 'Airedale Terrier') {
-        console.log('merge');
         imgID = 'airedale ';
     }
     else if(selector.value == 'Akbash Dog') {
-        console.log('merge');
         imgID = 'akbash ';
     }
     else if(selector.value == 'Akita') {
-        console.log('merge');
         imgID = 'akita ';
+    }
+    else {
+        imgID = '';
     }
 }
